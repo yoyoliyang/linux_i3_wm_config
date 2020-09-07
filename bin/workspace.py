3b2ef64807593fb5d73d897f7242607b42b60236
yoyoliyang@gmail.com <yoyoliyang@gmail.com>
2020-09-07 09:56:30 +0800

    左右参数修正

--- bin/workspace.py
+++ bin/workspace.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 import sys
+import os
+import json
 
-# 用法：使用执行该脚本并添加right的参数，则i3工作区切换到下一个，否则默认切换到上一个
+# 用法：使用执行该脚本并添加left的参数，则i3工作区切换到下一个，否则默认切换到下一个
 workspaces = os.popen('i3-msg -t get_workspaces')
-direction = sys.argv[1]
+
+try:
+    direction = sys.argv[1]
+except:
+    direction = 'right'
+
 json = json.load(workspaces)
 for ws in json:
     if ws['focused'] is True:
-        if direction == 'right':
+        if direction == 'left':
             next_ws = int(ws['name']) - 1
             os.system(f'i3-msg workspace {next_ws}')
             break
         next_ws = int(ws['name']) + 1
         os.system(f'i3-msg workspace {next_ws}')
         break
+print("需要left或者right作爲切換參數")
