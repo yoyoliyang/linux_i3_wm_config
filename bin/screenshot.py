6f990af538f42d4071e337d55e70185450e67069
yoyoliyang@gmail.com <yoyoliyang@gmail.com>
2020-09-07 09:49:18 +0800

    修正截图目录

--- bin/screenshot.py
+++ bin/screenshot.py
@@ -2,13 +2,13 @@
 import os
 import time
 
-PATH = '$HOME/images/'
+PATH = os.getenv('HOME')
 FONT = '--font "pango:WenQuanYi Zen Hei 20"'
 name = time.strftime('%c') + '.png'
-os.system(f'scrot {PATH}"{name}"')
-if os.path.lexists(PATH+name):
+os.system(f'scrot {PATH}/"{name}"')
+if os.path.lexists(PATH+"/"+name):
     os.system(
-        f'i3-nagbar -m "截图 {name} 保存到了 ~/images/" {FONT} & sleep 2 && kill `pidof i3-nagbar`')
+        f'i3-nagbar -t warning -m "截图 {name} 保存到了 {PATH}" {FONT} & sleep 2 && kill `pidof i3-nagbar`')
 else:
     os.system(
         f'i3-nagbar -m "截图失败" {FONT}')
